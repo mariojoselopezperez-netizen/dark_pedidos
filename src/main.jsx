f7280863e08b655aec388fea1e5fc06cea46c361
Mario José López Pérez <mariojoselopezperez@gmail.com>
2025-09-11 22:25:59 +0000

    subida

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter as Router } from 'react-router-dom'
 
+// Inicializa la app con HashRouter para que funcione en Android
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
 )
